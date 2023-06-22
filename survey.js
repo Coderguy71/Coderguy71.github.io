934196d016027cb2b5e78c8e31971177f11c2a2f
Coderguy71 <137341691+Coderguy71@users.noreply.github.com>
2023-06-22 16:12:12 -0400

    Update survey.js

--- survey.js
+++ survey.js
@@ -4,5 +4,5 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     // Perform any form processing/validation here
 
     // Redirect to the desired page
-    window.location.href = "https://example.com/redirect-page";
+    window.location.href = "formsubmission.html";
 });
