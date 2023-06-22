b286f567ba865082e1ec8b99887077bd6a76dd3b
Coderguy71 <137341691+Coderguy71@users.noreply.github.com>
2023-06-22 16:08:59 -0400

    Create survey.js

---
+++ survey.js
@@ -0,0 +1,8 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Perform any form processing/validation here
+
+    // Redirect to the desired page
+    window.location.href = "https://example.com/redirect-page";
+});
