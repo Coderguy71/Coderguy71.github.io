f07dd1acc4dca75d131cc0ed0e5f0176f73d851f
Coderguy71 <137341691+Coderguy71@users.noreply.github.com>
2024-01-15 14:28:45 -0500

    Delete survey.js

--- survey.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Perform any form processing/validation here
-
-    // Redirect to the desired page
-    window.location.href = "subfolder/formsubmission.html";
-});
